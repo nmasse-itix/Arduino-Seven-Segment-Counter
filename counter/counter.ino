abe2f929e052f93f0aa710112117453e9f2bcc31
Nicolas MASSE <nicolas.masse@itix.fr>
2018-04-08 20:03:08 +0200

    add a license

--- counter/counter.ino
+++ counter/counter.ino
@@ -1,10 +1,30 @@
 /*
-  A seven segement display counter
-
-  Counts the number of times a button is pressed and displays this number on a 7 segment display
-
-  Nicolas Massé
-*/
+ * A seven segement display counter
+ *
+ * Counts the number of times a button is pressed and displays this number on a 7 segment display
+ *
+ * MIT License
+ *
+ * Copyright (c) 2018 Nicolas Massé
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 
 /*
  * The button is connected to PIN 2.
